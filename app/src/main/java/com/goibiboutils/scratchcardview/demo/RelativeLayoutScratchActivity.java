d7097686f1f0dbbdfde703720ace3ed5e18f8168
Mohit Gupt <mohit.gupt@goibibo.com>
2019-08-05 14:21:09 +0530

    Added androidx support

--- app/src/main/java/com/goibiboutils/scratchcardview/demo/RelativeLayoutScratchActivity.java
+++ app/src/main/java/com/goibiboutils/scratchcardview/demo/RelativeLayoutScratchActivity.java
@@ -2,7 +2,7 @@ package com.goibiboutils.scratchcardview.demo;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.LayoutInflater;
 
 import com.goibibo.libs.views.ScratchRelativeLayoutView;
